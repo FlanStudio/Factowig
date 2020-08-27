b31ca44e279814b23a07ae28bd02431c88c4b1a1
Jonathan Molina <jony635molina@gmail.com>
2020-08-27 20:14:34 +0200

    Movement without rotation

---
+++ OverZombied/Assets/Scripts/Characters/InputController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InputController : MonoBehaviour
+{
+    public static InputController Instance;
+
+    public enum Controls { KeyboardMouse, Controller };
+
+    private Controls _controls = Controls.KeyboardMouse;
+    public Controls controls { get { return _controls; } }
+
+    public Gamepad gamepad = null;
+    public Keyboard keyboard = null;
+
+    private void Awake()
+    {
+        Instance = this;
+
+        keyboard = Keyboard.current;
+
+        if (Gamepad.current != null)
+        {
+            gamepad = Gamepad.current;
+            _controls = Controls.Controller;
+        }
+    }
+}
---
+++ OverZombied/Assets/Scripts/Characters/MovementController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class MovementController : MonoBehaviour
+{    
+    private Vector2 movementNorm = Vector2.zero;
+
+    public float speed = 15f;
+
+    private void Update()
+    {
+        movementNorm = Vector2.zero;
+
+        #region MOVEMENT VECTOR
+        switch (InputController.Instance.controls)
+        {
+            case InputController.Controls.KeyboardMouse:
+                {
+                    if (InputController.Instance.keyboard.wKey.isPressed)
+                        movementNorm += new Vector2(0, 1);
+                    if (InputController.Instance.keyboard.sKey.isPressed)
+                        movementNorm += new Vector2(0, -1);
+                    if (InputController.Instance.keyboard.aKey.isPressed)
+                        movementNorm += new Vector2(-1, 0);
+                    if (InputController.Instance.keyboard.dKey.isPressed)
+                        movementNorm += new Vector2(1, 0);
+
+                    break;
+                }
+                
+            case InputController.Controls.Controller:
+                {
+                    Vector2 value = Vector2.zero;
+                    value = InputController.Instance.gamepad.leftStick.ReadValue();
+
+                    movementNorm = value;
+                    
+                    break;
+                }
+        }
+        #endregion
+
+        #region APPLY MOVEMENT
+        transform.Translate(new Vector3(movementNorm.x, 0, movementNorm.y) * speed * Time.deltaTime);
+        #endregion
+    }
+}
