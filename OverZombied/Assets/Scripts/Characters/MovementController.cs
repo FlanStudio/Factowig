bdeb9fc2a8de9bb1e5152156d0bca881da235ef4
Jonathan Molina <jony635molina@gmail.com>
2020-08-31 09:30:40 +0200

    Character movement rotation

--- OverZombied/Assets/Scripts/Characters/MovementController.cs
+++ OverZombied/Assets/Scripts/Characters/MovementController.cs
@@ -8,7 +8,14 @@ public class MovementController : MonoBehaviour
     private Vector2 movementNorm = Vector2.zero;
 
     public float speed = 15f;
+    public float rotationSpeed = 10f;
 
+    private void Awake()
+    {
+        Debug.Log("Game Started");
+    }
+
+    [System.Obsolete]
     private void Update()
     {
         movementNorm = Vector2.zero;
@@ -43,7 +50,14 @@ public class MovementController : MonoBehaviour
         #endregion
 
         #region APPLY MOVEMENT
-        transform.Translate(new Vector3(movementNorm.x, 0, movementNorm.y) * speed * Time.deltaTime);
+        transform.Translate(new Vector3(movementNorm.x, 0, movementNorm.y) * speed * Time.deltaTime, Space.World);
+        #endregion
+
+        #region LOOK AHEAD
+
+        float diffAngle = Vector3.SignedAngle(transform.forward.normalized, new Vector3(movementNorm.x, 0, movementNorm.y), Vector3.up);
+        transform.Rotate(Vector3.up, diffAngle * rotationSpeed * Time.deltaTime);
+
         #endregion
     }
 }
