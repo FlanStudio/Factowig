ee7cd87dbf2990531e3e4c276e2bdc6ddd6f1738
Jonathan Molina <jony635molina@gmail.com>
2020-09-08 16:33:27 +0200

    ClientManager spawning chairs

---
+++ OverZombied/Assets/Scripts/ClientManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClientManager : MonoBehaviour
+{
+    public uint startEnabled = 2u;
+    public float totalTime = 10f;
+
+    public AnimationCurve activateChairsCurve;
+
+    [SerializeField]
+    private List<ClientBehavior> clients = new List<ClientBehavior>();
+
+    private uint enabledChairs = 0u;
+
+    private void Awake()
+    {
+        if(startEnabled <= clients.Count)
+        {
+            for(int i = 0; i < startEnabled; ++i)
+            {
+                clients[i].gameObject.SetActive(true);
+            }
+            enabledChairs = startEnabled;
+        }
+    }
+
+    private void Update()
+    {
+        float chairsPercent = activateChairsCurve.Evaluate(Time.time / totalTime);
+
+        uint amountChairs = (uint)Mathf.FloorToInt(chairsPercent * (clients.Count - startEnabled)) + startEnabled;
+
+        while(amountChairs > enabledChairs)
+        {
+            foreach(ClientBehavior chair in clients)
+            {
+                if(!chair.gameObject.activeSelf)
+                {
+                    chair.gameObject.SetActive(true);
+                    enabledChairs++;
+                    break;
+                }
+            }
+        }
+    }
+}
