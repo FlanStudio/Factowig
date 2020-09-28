1568e2ccd6204027c227f4e64795313e753848e1
Jonathan Molina <jony635molina@gmail.com>
2020-09-28 17:35:37 +0200

    New look at for clients canvas, clients ui redone

--- OverZombied/Assets/Scripts/LookAt.cs
+++ OverZombied/Assets/Scripts/LookAt.cs
@@ -5,10 +5,16 @@ using UnityEngine;
 public class LookAt : MonoBehaviour
 {
     public Transform target;
+    private Vector3 targetPosition;
+
+    private void Awake()
+    {
+        targetPosition = target.position;
+        targetPosition.x = transform.position.x;
+    }
 
     private void Update()
     {
-        transform.LookAt(target);
-        transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y, transform.rotation.eulerAngles.z);
+        transform.rotation = Quaternion.LookRotation((transform.position - targetPosition).normalized);
     }
 }
