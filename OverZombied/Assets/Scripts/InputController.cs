343ab0c3a8b298ef8b6d6c5e9966e55298c6603a
Jonathan Molina <jony635molina@gmail.com>
2020-09-29 11:43:36 +0200

    Dynamic input device detection + Singleplayer mode (Switch characters)

--- OverZombied/Assets/Scripts/InputController.cs
+++ OverZombied/Assets/Scripts/InputController.cs
@@ -1,5 +1,7 @@
-﻿using UnityEngine;
+﻿using System.Linq;
+using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Controls;
 
 public class InputController : MonoBehaviour
 {
@@ -16,27 +18,96 @@ public class InputController : MonoBehaviour
     public ControlsMode[] controlsMode { get; private set; } = new ControlsMode[4] {ControlsMode.Controller, ControlsMode.Controller, ControlsMode.Controller, ControlsMode.Controller};
     public PlayerInput[] playerInput = new PlayerInput[4] {new PlayerInput(), new PlayerInput(), new PlayerInput(), new PlayerInput()};
 
+    [SerializeField]
+    private MovementController[] movementControllers;
+
     private void Awake()
     {
         Instance = this;
 
+        SetInputs();
+
+        InputSystem.onDeviceChange +=
+        (device, change) =>
+        {
+            switch (change)
+            {
+                case InputDeviceChange.Added:                 
+                case InputDeviceChange.Disconnected:                 
+                case InputDeviceChange.Reconnected:                   
+                case InputDeviceChange.Removed:
+                default:
+                    SetInputs();
+                    break;
+            }
+        };
+    }
+
+    private void Update()
+    {
+        for(int i = 0; i < 4; ++i)
+        {
+            if(playerInput[i].keyboard != null)
+            {
+                if(playerInput[i].keyboard.anyKey.wasPressedThisFrame)
+                {
+                    controlsMode[i] = ControlsMode.KeyboardMouse;
+                }
+            }
+
+            if(playerInput[i].gamepad != null)
+            {
+                if(playerInput[i].gamepad.allControls.Any(x => x is ButtonControl && x.IsPressed() && !x.synthetic))
+                {
+                    controlsMode[i] = ControlsMode.Controller;
+                }
+            }
+        }
+
+        #region SINGLEPLAYER
+        if (Gamepad.all.Count < 2)
+            switch (controlsMode[0])
+            {
+                case ControlsMode.KeyboardMouse:
+                    if (playerInput[0].keyboard.tabKey.wasPressedThisFrame)
+                    {
+                        foreach(MovementController controller in movementControllers)
+                        {
+                            if (controller.playerID == 0)
+                                controller.playerID = 1;
+                            else
+                                controller.playerID = 0;
+                        }                       
+                    }
+                    break;
+                case ControlsMode.Controller:
+                    if (playerInput[0].gamepad != null && InputController.Instance.playerInput[0].gamepad.buttonNorth.wasPressedThisFrame)
+                    {
+                        foreach (MovementController controller in movementControllers)
+                        {
+                            if (controller.playerID == 0)
+                                controller.playerID = 1;
+                            else
+                                controller.playerID = 0;
+                        }                        
+                    }
+                    break;
+            }
+        #endregion
+    }
+
+    private void SetInputs()
+    {
         playerInput[0].keyboard = Keyboard.current;
         controlsMode[0] = ControlsMode.KeyboardMouse;
 
         for (int i = 0; i < 4; ++i)
-        {        
+        {
             if (i < Gamepad.all.Count)
             {
                 playerInput[i].gamepad = Gamepad.all[i];
                 controlsMode[i] = ControlsMode.Controller;
             }
         }
-
-        //Hardcoding
-        //playerInput[0].keyboard = Keyboard.current;
-        //controlsMode[0] = ControlsMode.KeyboardMouse;
-
-        //controlsMode[1] = ControlsMode.Controller;
-        //playerInput[1].gamepad = Gamepad.all[0];
     }
 }
