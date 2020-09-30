4a3f1bf3a109742f88328ace65b46e0b7748b1fe
Jonathan Molina <jony635molina@gmail.com>
2020-09-30 22:58:03 +0200

    Fix layout scaling with screen

---
+++ OverZombied/Assets/ScreenScaleChecker.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScreenScaleChecker : MonoBehaviour
+{
+    private Vector2 lastResolution;
+    private HorizontalLayoutGroup layoutGroup;
+
+    private void Awake()
+    {
+        lastResolution = new Vector2(Screen.width, Screen.height);
+        layoutGroup = GetComponent<HorizontalLayoutGroup>();   
+    }
+
+    private void Start()
+    {
+        CheckCanvasScale();
+    }
+
+    private void Update()
+    {
+        if (lastResolution.x != Screen.width || lastResolution.y != Screen.height)
+        {
+            lastResolution.x = Screen.width;
+            lastResolution.y = Screen.height;
+
+            CheckCanvasScale();
+        }
+    }
+
+    private void CheckCanvasScale()
+    {
+        bool[] lastStates = new bool[transform.childCount];
+        for (int i = 0; i < transform.childCount; ++i)
+        {
+            Transform child = transform.GetChild(i);
+            lastStates[i] = child.gameObject.activeSelf;
+            child.gameObject.SetActive(true);
+
+            for (int j = 0; j < child.childCount; ++j)
+            {
+                child.GetChild(j).gameObject.SetActive(true);
+            }
+        }
+
+        layoutGroup.childControlWidth = true;
+        layoutGroup.childControlHeight = true;
+        layoutGroup.childForceExpandWidth = true;
+        layoutGroup.childForceExpandHeight = true;
+
+        LayoutRebuilder.ForceRebuildLayoutImmediate(GetComponent<RectTransform>());
+
+        for (int i = 0; i < transform.childCount; ++i)
+        {
+            Transform child = transform.GetChild(i);
+            child.gameObject.SetActive(lastStates[i]);
+        }
+
+        layoutGroup.childControlWidth = false;
+        layoutGroup.childControlHeight = false;
+        layoutGroup.childForceExpandWidth = false;
+        layoutGroup.childForceExpandHeight = false;
+
+        for (int i = 0; i < transform.childCount; ++i)
+        {
+            Transform child = transform.GetChild(i);
+            RecipeUI recipe = child.GetComponent<RecipeUI>();
+            recipe.EnableChildsOnRecipe();
+        }
+    }
+}
---
+++ OverZombied/Assets/Scripts/Recipes/RecipeUI.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RecipeUI : MonoBehaviour
+{
+    private Recipe recipe;
+
+    private Image scissors;
+    private Image comb;
+
+    [HideInInspector]
+    public bool activated = false;
+
+    public void EnableChildsOnRecipe()
+    {
+        if(recipe != null)
+        {
+            scissors.gameObject.SetActive(false);
+            comb.gameObject.SetActive(false);
+
+            foreach (IngredientData ingredient in recipe.ingredients)
+            {
+                switch (ingredient.name)
+                {
+                    case "Scissors":
+                        scissors.gameObject.SetActive(true);
+                        break;
+                    case "Comb":
+                        comb.gameObject.SetActive(true);
+                        break;
+                }
+            }
+        }
+    }
+
+    public void SetRecipe(Recipe recipe)
+    {
+        this.recipe = recipe;
+
+        if (!scissors || !comb)
+        {
+            scissors = transform.GetChild(0).GetComponent<Image>();
+            comb = transform.GetChild(1).GetComponent<Image>();
+        }
+
+        EnableChildsOnRecipe();
+
+        gameObject.SetActive(true);
+    }
+}
