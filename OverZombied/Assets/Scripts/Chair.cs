2f2b89661eedd0b265537c5efc1cfd098af81872
Jonathan Molina <jony635molina@gmail.com>
2020-10-06 12:19:13 +0200

    Placing and removing wigs from chairs with animations

--- OverZombied/Assets/Scripts/Chair.cs
+++ OverZombied/Assets/Scripts/Chair.cs
@@ -4,17 +4,46 @@ using UnityEngine;
 
 public class Chair : MonoBehaviour
 {
+    public Animator animator;
+
     public MeshRenderer normalHair;
     public MeshRenderer cutHair;
     public MeshRenderer combedHair;
     public MeshRenderer cutcombedHair;
 
+    public MeshRenderer[] chairMeshes;
 
+    private Ingredient wig;
 
+    public void PlaceWig(Ingredient wig)
+    {
+        switch(wig.data.name)
+        {
+            case "Normal Hair":
+                normalHair.gameObject.SetActive(true);
+                break;
+            case "Cut Hair":
+                cutHair.gameObject.SetActive(true);
+                break;
+        }
 
+        animator.SetBool("Wig", true);
+        this.wig = wig;
+    }
 
+    public Ingredient RemoveWig()
+    {
+        normalHair.gameObject.SetActive(false);
+        cutHair.gameObject.SetActive(false);
+        combedHair.gameObject.SetActive(false);
+        cutcombedHair.gameObject.SetActive(false);
+        
+        animator.SetBool("Wig", false);
 
-
+        Ingredient ret = wig;
+        wig = null;
+        return ret;
+    }
 
     public void SelectMeshes()
     {
@@ -22,6 +51,11 @@ public class Chair : MonoBehaviour
         cutHair.material.color *= PlaceableSurface.selectedColorMultiplier;
         combedHair.material.color *= PlaceableSurface.selectedColorMultiplier;
         cutcombedHair.material.color *= PlaceableSurface.selectedColorMultiplier;   
+
+        foreach(MeshRenderer renderer in chairMeshes)
+        {
+            renderer.material.color *= PlaceableSurface.selectedColorMultiplier;
+        }
     }
 
     public void UnSelectMeshes()
@@ -30,5 +64,10 @@ public class Chair : MonoBehaviour
         cutHair.material.color /= PlaceableSurface.selectedColorMultiplier;
         combedHair.material.color /= PlaceableSurface.selectedColorMultiplier;
         cutcombedHair.material.color /= PlaceableSurface.selectedColorMultiplier;
+
+        foreach (MeshRenderer renderer in chairMeshes)
+        {
+            renderer.material.color /= PlaceableSurface.selectedColorMultiplier;
+        }
     }
 }
