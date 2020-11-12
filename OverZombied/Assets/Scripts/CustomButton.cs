8f0cdd93c334a6dbdf20547d62fe4afc73a41505
Jonathan Molina <jony635molina@gmail.com>
2020-11-12 17:58:33 +0100

    MainMenu interactable buttons

---
+++ OverZombied/Assets/Scripts/CustomButton.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class CustomButton : Button
+{
+    public Sprite normalSprite;
+    public Sprite hoverSprite;
+    public Sprite clickSprite;
+
+    private Image image = null;
+
+    private void Awake()
+    {
+        image = GetComponent<Image>();
+    }
+
+    public override void OnPointerDown(PointerEventData eventData)
+    {
+        base.OnPointerDown(eventData);
+
+    }
+
+    public override void OnPointerEnter(PointerEventData eventData)
+    {
+        base.OnPointerEnter(eventData);
+        image.sprite = hoverSprite;
+    }
+
+    public override void OnPointerExit(PointerEventData eventData)
+    {
+        base.OnPointerExit(eventData);
+        image.sprite = normalSprite;
+    }
+}
