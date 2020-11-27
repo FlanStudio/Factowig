e0e1484e924e86b22549afc4cea0ca0709b57f1b
Jonathan Molina <jony635molina@gmail.com>
2020-11-27 12:17:10 +0100

    experiments

---
+++ Factowig/Assets/test.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class test : MonoBehaviour
+{
+    private RectTransform rectTransform;
+
+    private void Awake()
+    {
+        rectTransform = GetComponent<RectTransform>();
+    }
+
+    private void Update()
+    {
+        if(Keyboard.current.digit1Key.wasPressedThisFrame)
+        {
+            rectTransform.anchoredPosition -= new Vector2(rectTransform.rect.width * 0.1f, 0f);
+        }
+    }
+}
